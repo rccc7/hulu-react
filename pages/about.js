49c910843018360835db1d0707a7c9cd289d4f3d
rccc7 <rccc@outlook.com>
2021-07-06 16:15:08 -0400

    Added about page and attributions

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ function about() {
                 <h1>Developed by RCCC 😎.</h1>
                 <p>This is just a clone for learning and testing purposes only.
                     Images and desccriptions obtained from TMDb API:</p>
-                <a href='https://www.themoviedb.org/' target="_blank">
+                <a href='https://www.themoviedb.org/' target="_blank" rel="noreferrer" >
                     <Image
                         className='object-contain'
                         src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg'
@@ -18,7 +18,7 @@ function about() {
                         height={100}
                     />
                 </a>
-                <p>HULU Logo obtained from <a className='text-blue-700' href='https://commons.wikimedia.org/wiki/File:Hulu_logo_2017.svg' target='_blank'>Wikimedia Commons</a></p>
+                <p>HULU Logo obtained from <a className='text-blue-700' href='https://commons.wikimedia.org/wiki/File:Hulu_logo_2017.svg' target='_blank' rel="noreferrer" >Wikimedia Commons</a></p>
             </div>
         </div>
     )
